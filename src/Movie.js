984b49061ffb778fcabb3f0c25ae24f245f9ef90
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-07-10 00:57:48 +0600

    Styled components was added

---
+++ src/Movie.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MovieSection from "./components/MovieSection";
+
+const Movie = () => {
+  return <MovieSection />;
+};
+
+export default Movie;
---
+++ src/components/MovieSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const StyledMovieSection = styled.section`
+  color: green;
+`;
+
+const MovieSection = () => {
+  return <StyledMovieSection>FrontEnd 30 - Movie App</StyledMovieSection>;
+};
+export default MovieSection;
